c73cc7c9bb4c811cd614eba68e5df3749ba2b08e
victor campos <64999643+cravcampos@users.noreply.github.com>
2022-12-27 23:15:04 -0500

    comentario sobre codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,7 +4,8 @@ public record PersonaRegistro (String nombre, String apellidos){
 
     @Override
     public String toString() {
-        System.out.println("Esta instruccion es inservible");
+        //victor comente la linea de codigo la cual considero eliminar
+        //System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
