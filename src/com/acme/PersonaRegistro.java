2a669a07f2e80081b3bffd66c73e02961d794872
Victor Campos <cravcampos@gmail.com>
2022-12-27 23:22:12 -0500

    se quito la linea de codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,8 +4,6 @@ public record PersonaRegistro (String nombre, String apellidos){
 
     @Override
     public String toString() {
-        //victor comente la linea de codigo la cual considero eliminar
-        //System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
